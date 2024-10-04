ada104cecb9b37a0cea55d5a3cef5c451b667b73
Seïn Bucher <sein.bucher@gmail.com>
2024-10-04 14:38:26 +0200

    manual animations

--- main.js
+++ main.js
@@ -6,17 +6,20 @@ const root = document.querySelector(':root'),
         input: document.querySelector('.form-input'),
         back: document.querySelector('.form-back'),
         next: document.querySelector('.form-next')
-    };
-let clone = document.createElement('div'),
-    prevIndex,
-    inputIndex = 0,
-    valid,
-    output = {
+    },
+    outputTemplate = {
         name: '',
         email: '',
         phone: '',
         message: ''
-    }
+    };
+let clone = document.createElement('div'),
+    prevIndex,
+    bounce = 8,
+    inputIndex = 0,
+    valid,
+    restart = false,
+    output = {...outputTemplate};
 
 function handleInput(e) {
     isInputValid()
@@ -25,16 +28,22 @@ function handleInput(e) {
 
 function textTransition(element, content) {
     if (element.textContent === content) return;
+    root.style.setProperty(`--calculated-x`, `${50}px`);
     element.parentNode.classList.add('transition')
     let transition = document.querySelector('.transition');
-    
+
     setTimeout(() => {
-        transition.classList.remove('transition');
+        root.style.setProperty(`--calculated-x`, `-${bounce}px`);
         transition.querySelector('p').textContent = content;
+        setTimeout(() => {
+            transition.classList.remove('transition');
+        }, 200);
+        
     }, 100);
 }
 
 function isInputValid() {
+    if (restart) return;
     valid = {
         name: form.input.value.length>1,
 
@@ -66,6 +75,7 @@ function isInputValid() {
             if (valid.email) {
                 form.dots[1].classList.add('active');
                 form.next.classList.add('active');
+                textTransition(form.next.querySelector('p'), 'Next')
             } else {
                 form.dots[1].classList.remove('active');
                 form.next.classList.remove('active');
@@ -98,13 +108,14 @@ function isInputValid() {
 }
 
 function changePage(e, next) {
-    
-    if (!e.target.classList.contains('active') || (next && inputIndex === 3)) return;
-    
+    if (!e.target.classList.contains('active') && !restart) return;
+    if (next && inputIndex === 3 && !restart) { sendForm(); return; };
     growText('')
-    prevIndex = inputIndex;
-    next ? inputIndex++ : inputIndex--;
-    next ? form.dots[inputIndex-1].classList.add('active') : form.dots[inputIndex+1].classList.remove('active'); 
+    if (!restart) {
+        prevIndex = inputIndex;
+        next ? inputIndex++ : inputIndex--;
+        next ? form.dots[inputIndex-1].classList.add('active') : form.dots[inputIndex+1].classList.remove('active'); 
+    }
     switch (inputIndex) {
         case 0: 
             form.input.name = "name"
@@ -133,13 +144,64 @@ function changePage(e, next) {
     form.dots[inputIndex].classList.add('current')
 }
 
+
+growText(form.input.value)
 function growText(value) {
+    let prevHeight = clone.offsetHeight;
     clone.dataset.replicatedValue = value;
     clone.classList.add('clone');
     form.grow.appendChild(clone);
-    root.style.setProperty(`--grow-after-maxHeight`, `${clone.offsetHeight}px`);
+    root.style.setProperty(`--grow-after-max-height`, `${clone.offsetHeight>prevHeight ? clone.offsetHeight+(bounce/2) : clone.offsetHeight}px`);
+    setTimeout(() => {
+        root.style.setProperty(`--grow-after-max-height`, `${clone.offsetHeight}px`);
+    }, 100);
 }
+
+function sendForm() {
+    form.next.querySelector('p').textContent = 'Restart'
+    let center = form.container.clientWidth/2 - (form.next.querySelector('span').clientWidth+12+form.next.querySelector('p').clientWidth/2)
+    root.style.setProperty(`--calculated-x`, `-${center+bounce}px`);
+    root.style.setProperty(`--calculated-y`, `-${form.input.clientHeight>70 ? bounce : 0}px`);
+    form.next.classList.remove('active')
+    form.back.classList.remove('active')
+    form.next.classList.add('transition')
+
+    form.dots[0].parentNode.style.transform = 'translateY(50px)'
+    form.dots[0].parentNode.style.opacity = 0;
+    form.dots.forEach(dot => dot.classList.remove('active'))
+    setTimeout(() => {
+        root.style.setProperty(`--calculated-x`, `-${center}px`);
+        root.style.setProperty(`--calculated-y`, `-${0}px`);
+    }, 200);
+
+    growText('');
+    form.input.value = 'Sent! We’ll contact you shortly.';
+    form.input.classList.add('active');
+
+    form.input.disabled = true;
+    output = {...outputTemplate};
+    prevIndex = inputIndex;
+    inputIndex = 0;
+    restart = true;
+}
+
+function restartForm(e) {
+    
+    
+    changePage(e, true)
+    form.input.classList.remove('active');
+    form.dots[0].parentNode.style.transform = 'unset';
+    form.dots[0].parentNode.style.opacity = 1;
+    form.next.querySelector('p').textContent = 'Next';
+    form.next.classList.remove('transition');
+
+    restart = false;
+    form.input.disabled = false;
+    
+    
+}
+
 
 form.input.addEventListener('input', handleInput)
 form.back.addEventListener('click', (e) => changePage(e, false))
-form.next.addEventListener('click', (e) => changePage(e, true))
+form.next.addEventListener('click', (e) => restart ? restartForm(e) : changePage(e, true))
